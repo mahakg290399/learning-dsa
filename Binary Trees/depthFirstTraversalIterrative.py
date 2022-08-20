891840ea5741e9d38d965b8548a6355eb1092997
Mahak Goyal <mahakg290399@gmail.com>
2022-08-20 20:45:09 +0530

    iterative Depth First Traversal

---
+++ Binary Trees/depthFirstTraversalIterrative.py
@@ -0,0 +1,17 @@
+from locale import currency
+import binaryTree as bt
+
+def treeValues(node):
+    if node.val == None: return False
+    stack=[node]
+    
+    while(len(stack)>0):
+        current = stack.pop()
+        if current.right : stack.append(current.right)
+        if current.left : stack.append(current.left)
+        print(current.val)
+
+
+
+
+treeValues(bt.a)
