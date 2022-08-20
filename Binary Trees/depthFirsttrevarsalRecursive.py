c04e2a5d5b11d8eb31b16b2e879a555596f441fa
Mahak Goyal <mahakg290399@gmail.com>
2022-08-20 22:14:24 +0530

    fixed some problem with recursive solution

--- Binary Trees/depthFirsttrevarsalRecursive.py
+++ Binary Trees/depthFirsttrevarsalRecursive.py
@@ -1,11 +1,16 @@
 import binaryTree as bt
 
 #recursive
-def treeValues(node):
-    if not(node.val): return False
-    print(node.val)
-    if node.left :treeValues(node.left)
-    if node.right :treeValues(node.right)
-    return True
 
-treeValues(bt.a)
+def treeValues(node):
+    # if not(node.val): return False
+    # print(node.val)
+    # if node.left :treeValues(node.left)
+    # if node.right :treeValues(node.right)
+    # return True
+    if node == None: return []
+    leftValues = treeValues(node.left)
+    rightValues = treeValues(node.right)
+    return [node.val, *leftValues, *rightValues]
+
+print(treeValues(bt.a))
