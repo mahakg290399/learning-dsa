8fefb1892261bc0c92ee7cde5c53ab87b5ebbc8d
Mahak Goyal <mahakg290399@gmail.com>
2022-10-23 22:52:46 +0530

    Found data algo

--- LinkedList/FindData.py
+++ LinkedList/FindData.py
@@ -1,1 +1,20 @@
+from locale import currency
 import node
+
+#iterative
+def findDataIterative(head, target):
+    current = head
+    while current:
+        if current.val == target:
+            return "Found"
+        current = current.next
+    return "Not Found"
+
+# Recursive
+def findDataRecursive(head, target):
+    if not head: return "Not Found"
+    if head.val == target: return "Found"
+    return findDataRecursive(head.next, target)
+
+print(findDataIterative(node.a, "E"))
+print(findDataRecursive(node.a, "C"))
