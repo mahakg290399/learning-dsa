0465aebc1c2d1ec4b835af445849c77aafdb9c80
Mahak Goyal <mahakg290399@gmail.com>
2025-09-01 11:32:34 +0530

    updated to a clearer code

--- matrix/TransposeandSymetricCheck.py
+++ matrix/TransposeandSymetricCheck.py
@@ -4,21 +4,18 @@
 # and then check if the matrix is symmetric or not by checking if the matrix equate with it's 
 # transpose. if it does then the given matrix is Symmetrix, otherwise it is not.
 
-def buildMatrix(liss):
-    breakCount = liss.count('<>')
-    matrix = []
-    temp = []
-    count = 0
-    for i in liss:
-        if i == '<>':
-            matrix.append(temp)
-            temp = []
-            continue
-        temp.append(i)
-    matrix.append(temp)
-    return matrix
+def buildMatrix(liss:list):
+    liss.append('<>')
+    result = [] 
+    row= []
+    for num in liss:
+        if num != '<>': row.append(num)
+        else: 
+            result.append(row)
+            row = []
+    return result
 
-def transpose(matrix):
+def transpose(matrix:list):
     row = len(matrix)
     column = len(matrix[0])
     result = [['0' for i in range(row)] for j in range(column)]
@@ -26,9 +23,9 @@ def transpose(matrix):
         for j in range(row):
             result[i][j] = matrix[j][i]
     return result
-inputList = ["1","2",'<>','2','1','<>','3','1','<>','4','4']
 
 
+inputList = ['1','2','<>','2','1','<>','3','1','<>','4','4']
 matrix = buildMatrix(inputList)
 print(matrix)
 print(transpose(matrix))
